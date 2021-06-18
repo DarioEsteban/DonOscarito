843848e40aafd7d0d06afa37ab3a40138fc35113
DarioEsteban <darioesteban.verdugo@gmail.com>
2021-06-18 15:04:27 -0400

    Proyecto

---
+++ src/main/java/com/usm/donoscarito/DonOscaritoApplication.java
@@ -0,0 +1,13 @@
+package com.usm.donoscarito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonOscaritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonOscaritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usm/donoscarito/DonOscaritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usm.donoscarito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonOscaritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
