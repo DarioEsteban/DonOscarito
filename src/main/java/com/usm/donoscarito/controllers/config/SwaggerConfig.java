1d1ab80e1061d0febf65d23a35cf9a5e24fc2ef9
DarioEsteban <darioesteban.verdugo@gmail.com>
2021-06-19 21:16:17 -0400

    - Se modifica swagger para futuro (cambiará)

--- src/main/java/com/usm/donoscarito/controllers/config/SwaggerConfig.java
+++ src/main/java/com/usm/donoscarito/controllers/config/SwaggerConfig.java
@@ -1,15 +1,25 @@
 package com.usm.donoscarito.controllers.config;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.service.TokenEndpoint;
+import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -23,8 +33,9 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.basePackage("com.usm.donoscarito.controllers"))
 				.paths(PathSelectors.any())
 				.build()
-				.apiInfo(getApiInfo())
-				;
+				.securitySchemes(Arrays.asList(securityScheme()))
+				.securityContexts(Arrays.asList(securityContext()))
+				.apiInfo(getApiInfo());
 	}
 	
 	private ApiInfo getApiInfo() {
@@ -38,5 +49,35 @@ public class SwaggerConfig {
 				"",
 				Collections.emptyList()
 				);
+	}
+	
+	private SecurityScheme securityScheme() {
+	    GrantType grantType = new AuthorizationCodeGrantBuilder()
+	        .tokenEndpoint(new TokenEndpoint("" + "/token", "oauthtoken"))
+	        .tokenRequestEndpoint(
+	          new TokenRequestEndpoint("" + "/authorize", "CLIENT_ID", "CLIENT_SECRET"))
+	        .build();
+
+	    SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+	        .grantTypes(Arrays.asList(grantType))
+	        .scopes(Arrays.asList(scopes()))
+	        .build();
+	    return oauth;
+	}
+	
+	private AuthorizationScope[] scopes() {
+	    AuthorizationScope[] scopes = { 
+	      new AuthorizationScope("read", "for read operations"), 
+	      new AuthorizationScope("write", "for write operations"), 
+	      new AuthorizationScope("foo", "Access foo API") };
+	    return scopes;
+	}
+	
+	private SecurityContext securityContext() {
+	    return SecurityContext.builder()
+	      .securityReferences(
+	        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+	      .forPaths(PathSelectors.regex("/foos.*"))
+	      .build();
 	}
 }
