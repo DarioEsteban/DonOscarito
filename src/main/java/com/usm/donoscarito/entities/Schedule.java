c2143ea96197bbe92aef7658f6dcaa5dcca6d98c
matigaete <matias.gaetep@sansano.usm.cl>
2021-06-19 21:27:54 -0400

    Mini commit
    Me dio paja continuar, dejé algunas entidades listas, agradecido con el de arriba

--- src/main/java/com/usm/donoscarito/entities/Schedule.java
+++ src/main/java/com/usm/donoscarito/entities/Schedule.java
@@ -1,5 +1,35 @@
 package com.usm.donoscarito.entities;
 
 public class Schedule {
-
-}
+	private int idField;
+	private int idStateField;
+	private int idFieldType;
+	private int value;
+	
+	public int getIdField() {
+		return idField;
+	}
+	public void setIdField(int idField) {
+		this.idField = idField;
+	}
+	public int getIdStateField() {
+		return idStateField;
+	}
+	public void setIdStateField(int idStateField) {
+		this.idStateField = idStateField;
+	}
+	public int getIdFieldType() {
+		return idFieldType;
+	}
+	public void setIdFieldType(int idFieldType) {
+		this.idFieldType = idFieldType;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+} 
