06fa8de1c9e0979ecb6d8b5741f7771b60da533a
FelipeM <felipe85.martinez@gmail.com>
2021-06-25 23:06:22 -0400

    ADD:User entities

--- src/main/java/com/usm/donoscarito/entities/User.java
+++ src/main/java/com/usm/donoscarito/entities/User.java
@@ -1,5 +1,96 @@
 package com.usm.donoscarito.entities;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "usuario")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private Integer idUser;
+	
+	@ManyToOne
+    @JoinColumn(name  = "id_tipo_usuario")
+	private UserType userType;
+
+	@Column(name="nombre")
+	private String name;
+	
+	@Column(name="apellido")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="telefono")
+	private String phone;
+	
+	@Column(name="contrasena")
+	private String password;
+	
+	public Integer getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
