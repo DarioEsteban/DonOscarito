42433fc3b252e364831bd0de03e5c91f00fa1894
FelipeM <felipe85.martinez@gmail.com>
2021-07-06 13:51:45 -0400

    se reestructura tabla reserva, se agrega llave primaria y foraneas

--- DB/db_oscarito.sql
+++ DB/db_oscarito.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-07-2021 a las 06:42:32
+-- Tiempo de generación: 06-07-2021 a las 19:50:36
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.7
 
@@ -107,24 +107,24 @@ CREATE TABLE `horario_disponible` (
 --
 
 INSERT INTO `horario_disponible` (`id_bloque`, `id_cancha`, `fecha`, `bloque_inicio`, `bloque_fin`, `estado`) VALUES
-(0, 1, '2021-06-26', '10:00:00', '11:00:00', 1),
-(0, 2, '2021-06-26', '10:00:00', '11:00:00', 1),
-(0, 1, '2021-06-26', '11:00:00', '12:00:00', 0),
-(0, 2, '2021-06-26', '11:00:00', '12:00:00', 1),
-(0, 1, '2021-06-26', '12:00:00', '13:00:00', 1),
-(0, 2, '2021-06-26', '12:00:00', '13:00:00', 1),
-(0, 1, '2021-06-26', '13:00:00', '14:00:00', 0),
-(0, 2, '2021-06-26', '13:00:00', '14:00:00', 1),
-(0, 1, '2021-06-26', '14:00:00', '15:00:00', 1),
-(0, 2, '2021-06-26', '14:00:00', '15:00:00', 1),
-(0, 1, '2021-06-26', '15:00:00', '16:00:00', 1),
-(0, 2, '2021-06-26', '15:00:00', '16:00:00', 1),
-(0, 1, '2021-06-26', '16:00:00', '17:00:00', 1),
-(0, 2, '2021-06-26', '16:00:00', '17:00:00', 1),
-(0, 1, '2021-06-26', '17:00:00', '18:00:00', 1),
-(0, 2, '2021-06-26', '17:00:00', '18:00:00', 1),
-(0, 1, '2021-06-26', '18:00:00', '19:00:00', 1),
-(0, 2, '2021-06-26', '18:00:00', '19:00:00', 1);
+(1, 1, '2021-06-26', '10:00:00', '11:00:00', 1),
+(2, 2, '2021-06-26', '10:00:00', '11:00:00', 1),
+(3, 1, '2021-06-26', '11:00:00', '12:00:00', 0),
+(4, 2, '2021-06-26', '11:00:00', '12:00:00', 1),
+(5, 1, '2021-06-26', '12:00:00', '13:00:00', 1),
+(6, 2, '2021-06-26', '12:00:00', '13:00:00', 1),
+(7, 1, '2021-06-26', '13:00:00', '14:00:00', 0),
+(8, 2, '2021-06-26', '13:00:00', '14:00:00', 1),
+(9, 1, '2021-06-26', '14:00:00', '15:00:00', 1),
+(10, 2, '2021-06-26', '14:00:00', '15:00:00', 1),
+(11, 1, '2021-06-26', '15:00:00', '16:00:00', 1),
+(12, 2, '2021-06-26', '15:00:00', '16:00:00', 1),
+(13, 1, '2021-06-26', '16:00:00', '17:00:00', 1),
+(14, 2, '2021-06-26', '16:00:00', '17:00:00', 1),
+(15, 1, '2021-06-26', '17:00:00', '18:00:00', 1),
+(16, 2, '2021-06-26', '17:00:00', '18:00:00', 1),
+(17, 1, '2021-06-26', '18:00:00', '19:00:00', 1),
+(18, 2, '2021-06-26', '18:00:00', '19:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -262,20 +262,75 @@ ALTER TABLE `estado_reserva`
   ADD PRIMARY KEY (`id_estado_reserva`);
 
 --
+-- Indices de la tabla `horario_disponible`
+--
+ALTER TABLE `horario_disponible`
+  ADD PRIMARY KEY (`id_bloque`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id_pago`);
+
+--
 -- Indices de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  ADD PRIMARY KEY (`id_reserva`);
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `pk_id_cancha` (`id_cancha`),
+  ADD KEY `pk_id_usuario` (`id_usuario`),
+  ADD KEY `pk_id_pago` (`id_pago`),
+  ADD KEY `pk_estado_reserva` (`id_estado_reserva`),
+  ADD KEY `pk_id_bloque` (`id_bloque`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `horario_disponible`
+--
+ALTER TABLE `horario_disponible`
+  MODIFY `id_bloque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
   MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `pk_estado_reserva` FOREIGN KEY (`id_estado_reserva`) REFERENCES `estado_reserva` (`id_estado_reserva`),
+  ADD CONSTRAINT `pk_id_bloque` FOREIGN KEY (`id_bloque`) REFERENCES `horario_disponible` (`id_bloque`),
+  ADD CONSTRAINT `pk_id_cancha` FOREIGN KEY (`id_cancha`) REFERENCES `cancha` (`id_cancha`),
+  ADD CONSTRAINT `pk_id_pago` FOREIGN KEY (`id_pago`) REFERENCES `pago` (`id_pago`),
+  ADD CONSTRAINT `pk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
