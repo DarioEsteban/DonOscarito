e86ea41295eb7370ed4180d61577e01f5ee56416
FelipeM <felipe85.martinez@gmail.com>
2021-07-13 01:07:00 -0400

    Se agrega add:mod roles de usuario

--- src/main/java/com/ums/donoscarito/controllers/UserTypeController.java
+++ src/main/java/com/ums/donoscarito/controllers/UserTypeController.java
@@ -2,10 +2,16 @@ package com.ums.donoscarito.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.usm.donoscarito.entities.UserType;
 
 public interface UserTypeController {
 
-	public List<UserType> getUsersTypes();	
+	public List<UserType> getUsersTypes();
+
+	public ResponseEntity<String> addUserType(UserType userType);
+
+	public ResponseEntity<String> updateUserType(UserType userType);	
 	
 }
--- src/main/java/com/usm/donoscarito/serviceImpl/UserTypeServiceImpl.java
+++ src/main/java/com/usm/donoscarito/serviceImpl/UserTypeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.usm.donoscarito.serviceImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.usm.donoscarito.entities.User;
 import com.usm.donoscarito.entities.UserType;
 import com.usm.donoscarito.repository.UserTypeRepository;
 import com.usm.donoscarito.service.UserTypeService;
@@ -19,6 +22,32 @@ public class UserTypeServiceImpl implements UserTypeService {
 		// TODO Auto-generated method stub
 		return userTypeRepository.findAll();
 	}
-	
-	
+
+	@Override
+	public void update(UserType userType) {
+		// Validar existencia de elemento
+		if (userTypeRepository.existsById(userType.getIdUserType())) {
+			//Obtiene el registro que se quiere modificar
+			Optional<UserType> userTypeToUpdate = userTypeRepository.findById(userType.getIdUserType());
+			if(userTypeToUpdate.isPresent()) {
+				// Configurar clase
+				UserType userTypeUpdate = userTypeToUpdate.get();
+				
+				if(userType.getDescription() != null) {
+					userTypeUpdate.setDescription(userType.getDescription());
+				}
+				// Modificar
+				userTypeRepository.save(userTypeUpdate);
+			}
+			
+		} else {
+			throw new IllegalArgumentException("No existe Usuario.");
+		}
+		
+	}
+
+	@Override
+	public void save(UserType userType) {
+		userTypeRepository.save(userType);	
+	}
 }
